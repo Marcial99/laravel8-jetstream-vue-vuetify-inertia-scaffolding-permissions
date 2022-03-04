1f882d9816195b167122d5d734e6f26d51899699
PabloMarcial99 <17031116@itcelaya.edu.mx>
2022-03-03 18:16:21 -0600

    Laravel 8 with jetstream, vue, vuetify, intertia scaffolding

---
+++ resources/js/Pages/test.vue
@@ -0,0 +1,46 @@
+<!-- App.vue -->
+<template>
+    <v-app>
+        <v-navigation-drawer app v-model="drawer">
+            <!-- -->
+        </v-navigation-drawer>
+
+        <v-app-bar app>
+            <v-app-bar-nav-icon
+                @click.stop="drawer = !drawer"
+            ></v-app-bar-nav-icon>
+            <!-- -->
+        </v-app-bar>
+
+        <!-- Sizes your content based upon application components -->
+        <v-main>
+            <!-- Provides the application the proper gutter -->
+            <v-container fluid>
+                <v-parallax
+                    src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+                ></v-parallax>
+                <v-sheet
+                    color="white"
+                    elevation="3"
+                    height="1480"
+                    rounded
+                    width="auto"
+                ></v-sheet>
+            </v-container>
+        </v-main>
+
+        <v-footer app>
+            <!-- -->
+        </v-footer>
+    </v-app>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        drawer: false,
+    }),
+};
+</script>
+
+<style></style>
